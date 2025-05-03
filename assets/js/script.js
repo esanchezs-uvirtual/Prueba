e98cfefcf3998882de4f7c5f10ce1f862184bce7
Esteban Sanchez <esanchezs@uvirtual.ac.cr>
2025-05-03 15:28:23 -0600

    Primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let titulo = document.getElementById("titulo");
+titulo.textContent = "Nuevo título";
+
+let boton = document.getElementById("miBoton");
+boton.addEventListener("click", function() {
+  alert("Botón clickeado");
+});
